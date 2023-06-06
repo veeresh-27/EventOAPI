aa1d9fbd52dbbf4e7113e1aed840ab0b292f208d
veeresh-27 <veereshrm789@gmail.com>
2023-06-06 15:14:56 +0530

    All transient Added

--- Program.cs
+++ Program.cs
@@ -8,15 +8,18 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddTransient<EventService, EventService>();
 builder.Services.AddTransient<AuthService,AuthService>();
-builder.Services.AddTransient<SpaceServices,SpaceServices>();
 builder.Services.AddTransient<AdminServices, AdminServices>();
 builder.Services.AddTransient<ChatServices, ChatServices>();
 builder.Services.AddTransient<CommunitiesServices, CommunitiesServices>();
 builder.Services.AddTransient<CommunityMemberServices, CommunityMemberServices>();
 builder.Services.AddTransient<EventAttendeesServices, EventAttendeesServices>();
+builder.Services.AddTransient<EventsServices, EventsServices>();
+
 builder.Services.AddTransient<LikeServices, LikeServices>();
 builder.Services.AddTransient<PostServices, PostServices>();
+builder.Services.AddTransient<SpaceServices, SpaceServices>();
 
+builder.Services.AddTransient<UserServices, UserServices>();
 builder.Services.AddDbContext<EventContext>();
 builder.Services.AddTransient(typeof(EventContext));
 
